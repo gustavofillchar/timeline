a22c1831a638ee1679030480c6f8760ee48193f2
Gustavo Lima <guslimadev@gmail.com>
2025-08-07 21:34:54 -0300

    Refactor project structure and update title

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import timelineItems from "./timelineItems.js";
 import assignLanes from "./assignLanes.js";
-import Header from "./components/header";
+import Header from "./components/header/index.js";
 import Timeline from "./components/timeline";
 
 function App() {
